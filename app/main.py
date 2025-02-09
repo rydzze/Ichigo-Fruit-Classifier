466d150d5e8eb6e81ea76ebc6eb249a6a2e90b36
Rydzze <rydzze@gmail.com>
2025-02-09 14:55:35 +0800

    final commit

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet50 import preprocess_input
 
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 model = load_model('app/model.h5', custom_objects={'preprocess_input': preprocess_input})
 
 def allowed_file(filename, allowed_extensions):
